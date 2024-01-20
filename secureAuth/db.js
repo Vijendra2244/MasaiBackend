59777268bc9a5c9669943f76f57033f92b973622
Vijendra Chouhan <139648194+Vijendra2244@users.noreply.github.com>
2024-01-20 09:26:13 +0530

    Delete secureAuth directory

--- secureAuth/db.js
+++
@@ -1,7 +0,0 @@
-// const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
-// dotenv.config();
-
-// const connection = mongoose.connect(process.env.DATABASE_URL);
-
-// module.exports = connection;
