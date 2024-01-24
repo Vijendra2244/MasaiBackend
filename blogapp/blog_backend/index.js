92639b52acc49c825e2fd4f71bd63ddbb76cfe46
vijendra2244 <vs230267@gmail.com>
2024-01-24 13:38:29 +0530

    Added

--- blogapp/blog_backend/index.js
+++ blogapp/blog_backend/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://127.0.0.1:5173/",
+  origin: "*",
   credentials: true,
 };
 app.use(cors(corsOptions));
